335a941fad30e2ce0dfe783062f52eb5c55009af
ayyesu <iamdanielkissiedu@gmail.com>
2023-06-07 13:40:05 +0000

    added: layout view

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.todolistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem(v);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        
+        deleteListViewListener();
+    }
+
+    private void deleteListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+                Context context = getApplicationContext();
+                // Sending a toast message after an item is Deleted
+                Toast.makeText(context, "Item Deleted", Toast.LENGTH_SHORT).show();
+                // Deleting item at a particular position
+                items.remove(position);
+                // making sure list is up to date
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View v) {
+    }
+}
